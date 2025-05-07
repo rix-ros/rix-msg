b4d559408e69445e36eeef4247de0f1489b8af8e
Broderick <broderio@umich.edu>
2025-05-07 12:14:14 -0400

    add message hash index file for rixinfo support

--- rixmsg/src/main.py
+++ rixmsg/src/main.py
@@ -3,8 +3,21 @@ import os
 import glob
 import argparse
 
-from validate_json import validate_json, get_schema, write_to_file, get_hash
-from type_regex import is_static_array, is_dynamic_array, is_base_type, get_static_array_size, get_type, add_flags_to_fields
+from validate_json import (
+    validate_json,
+    get_schema,
+    write_to_file,
+    get_hash,
+    get_dict_from_json,
+)
+from type_regex import (
+    is_static_array,
+    is_dynamic_array,
+    is_base_type,
+    get_static_array_size,
+    get_type,
+    add_flags_to_fields,
+)
 from create_cpp import create_rixmsg_cpp
 from create_js import create_rixmsg_js
 from create_py import create_rixmsg_py
@@ -18,47 +31,82 @@ Functions:
   package <package name> - Show all messages names contained within a package
   create <path>          - Create implementation files from RIX message 
                            definition files
+  index                  - Index the RIX message definitions
 """
 
-def show(rixmsg):
-    package, msg = rixmsg.split('/')
 
-    filepath = f'{ROOT}/.rix/rixmsg/defs/{package}/{msg}.json'
+def index():
+    """
+    Writes the index file for all of the current RIX message definitions in ~/.rix/rixmsg/defs.
+    Sorted by hash.
+    Format:
+    <hash> <package>/<message>
+    """
+    index_file = f"{ROOT}/.rix/rixmsg/index.txt"
+    hashes_sorted = []
+    hash_to_msg = {}
+    for package in os.listdir(f"{ROOT}/.rix/rixmsg/defs/"):
+        package_path = f"{ROOT}/.rix/rixmsg/defs/{package}"
+        for msg in os.listdir(package_path):
+            if msg.endswith(".json"):
+                msg_data = get_dict_from_json(f"{package_path}/{msg}")
+                hash_value = msg_data["hash"]
+                hashes_sorted.append(hash_value)
+                hash_to_msg[hash_value] = f"{package}/{msg[:-5]}"
+    hashes_sorted = sorted(hashes_sorted)
+    with open(index_file, "w") as f:
+        for hash_value in hashes_sorted:
+            f.write(f"{hash_value} {hash_to_msg[hash_value]}\n")
+
+
+def show(rixmsg):
+    package, msg = rixmsg.split("/")
+
+    filepath = f"{ROOT}/.rix/rixmsg/defs/{package}/{msg}.json"
     if not os.path.exists(filepath):
-        print(f'Error: Package {package} does not exist')
+        print(f"Error: Package {package} does not exist")
         sys.exit(1)
 
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         print(file.read())
 
+
 def list_packages():
-    packages = os.listdir(f'{ROOT}/.rix/rixmsg/defs/')
+    packages = os.listdir(f"{ROOT}/.rix/rixmsg/defs/")
     for package in packages:
         print(package)
 
+
 def show_package(package):
     package = package.lower()
-    package_path =f'{ROOT}/.rix/rixmsg/defs/{package}'
+    package_path = f"{ROOT}/.rix/rixmsg/defs/{package}"
 
     if not os.path.isdir(package_path):
-        print(f'Error: Package {package} does not exist')
+        print(f"Error: Package {package} does not exist")
         sys.exit(1)
 
     msgs = os.listdir(package_path)
     for msg in msgs:
-        print(msg.split('.')[0])
+        print(msg.split(".")[0])
+
 
 def create(input_path: str) -> None:
     # Get the schema for validation
     schema = get_schema(f"{ROOT}/.rix/rixmsg/schema.json")
 
-    # Get a list of XML elements
+    # Get a list of JSON elements
     json_elements = []
     if os.path.isfile(input_path):
-        json_elements.append(validate_json(input_path, schema))
+        msg = validate_json(input_path, schema)
+        if "hash" not in msg:
+            msg["hash"] = get_hash(input_path)
+        json_elements.append(msg)
     elif os.path.isdir(input_path):
-        for json_file in glob.glob(os.path.join(input_path, '*.json')):
-            json_elements.append(validate_json(json_file, schema))
+        for json_file in glob.glob(os.path.join(input_path, "*.json")):
+            msg = validate_json(json_file, schema)
+            if "hash" not in msg:
+                msg["hash"] = get_hash(json_file)
+            json_elements.append(msg)
     else:
         print(f"{input_path} is not a valid file or directory.")
 
@@ -69,12 +117,9 @@ def create(input_path: str) -> None:
         def_file_path = package_dir + f"{msg['name']}.json"
         write_to_file(def_file_path, msg)
 
-        # Add the hash to the msg
-        msg['hash'] = get_hash(msg)
-
         # Add flags to the fields
-        add_flags_to_fields(msg['fields'])
-        
+        add_flags_to_fields(msg["fields"])
+
         # Generate the C++ implementation files
         dir_name = f"{ROOT}/.rix/include/rix/msg/{msg['package']}/"
         os.makedirs(dir_name, exist_ok=True)
@@ -101,33 +146,46 @@ def create(input_path: str) -> None:
         with open(file_name, "w") as f:
             f.write(create_rixmsg_py(msg))
 
+    # Re-index the definitions
+    index()
+
+
 def main(args):
-    if args.function == 'show':
+    if args.function == "show":
         if not args.arg:
             print("Error: show requires a rixmsg file as an argument. Use -h for help")
             sys.exit(1)
         show(args.arg)
-    elif args.function == 'packages':
+    elif args.function == "packages":
         list_packages()
-    elif args.function == 'package':
+    elif args.function == "package":
         if not args.arg:
-            print("Error: package requires a package name as an argument. Use -h for help")
+            print(
+                "Error: package requires a package name as an argument. Use -h for help"
+            )
             sys.exit(1)
         show_package(args.arg)
-    elif args.function == 'create':
+    elif args.function == "create":
         if not args.arg:
             print("Error: create requires a path as an argument. Use -h for help")
             sys.exit(1)
         create(args.arg)
+    elif args.function == "index":
+        index()
     else:
-        print(f'Error: Unknown function {args.function}. Use -h for help')
+        print(f"Error: Unknown function {args.function}. Use -h for help")
         sys.exit(1)
- 
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='rixmsg command line tool', usage=USAGE)
-    parser.add_argument('-v', '--version', action='version', version='rixmsg 1.0')
-    parser.add_argument('function', type=str, help='Function to call (show, packages, package, create)')
-    parser.add_argument('arg', type=str, nargs='?', help='Argument for the function')
+    parser = argparse.ArgumentParser(
+        description="rixmsg CLI", usage=USAGE
+    )
+    parser.add_argument("-v", "--version", action="version", version="rixmsg 1.0")
+    parser.add_argument(
+        "function", type=str, help="Function to call (show, packages, package, create)"
+    )
+    parser.add_argument("arg", type=str, nargs="?", help="Argument for the function")
     args = parser.parse_args()
-    
+
     main(args)
--- rixmsg/src/validate_json.py
+++ rixmsg/src/validate_json.py
@@ -2,19 +2,28 @@ from jsonschema import validate
 import json
 from hashlib import md5
 
+
 def get_schema(json_file: str) -> dict:
-    with open(json_file, 'r') as f:
+    with open(json_file, "r") as f:
         return json.load(f)
 
-def get_hash(msg: dict) -> str:
-    return md5(json.dumps(msg, sort_keys=True).encode()).hexdigest()
+
+def get_hash(pathname: str) -> str:
+    with open(pathname, "r") as f:
+        return md5(f.read().encode()).hexdigest()
+
 
 def write_to_file(filepath: str, data: dict) -> None:
-    with open(filepath, 'w') as f:
+    with open(filepath, "w") as f:
         f.write(json.dumps(data, indent=2))
 
+
 def validate_json(json_file: str, schema: dict) -> dict:
-    with open(json_file, 'r') as f:
+    with open(json_file, "r") as f:
         data = json.load(f)
         validate(data, schema)
         return data
+
+def get_dict_from_json(json_file: str) -> dict:
+    with open(json_file, "r") as f:
+        return json.load(f)
