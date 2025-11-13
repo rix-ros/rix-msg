5d7e604fbd5a29767e8e874877c11fa8316a8273
Broderick Riopelle <riopellebroderick@gmail.com>
2025-11-13 14:39:14 -0500

    rename src to rixmsg, remove references to deprecated JavaScript support

--- rixmsg/tests/test_create_py.py
+++ rixmsg/tests/test_create_py.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-os.sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src")
+os.sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../rixmsg")
 from message import Message
 from create_py import *
 
